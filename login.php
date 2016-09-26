e6038519f3f1da8e755272fbf801f5d9bc163bcd
Naseath Saly <naseath@web-essentials.asia>
2016-09-26 14:37:45 +0700

    [TASK] Edit login label

--- login.php
+++ login.php
@@ -55,7 +55,7 @@
 	
       <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login PHP TESTING PAGE</b></div>
 				
             <div style = "margin:30px">
                
